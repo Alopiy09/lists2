6cf882d91b4d7e4bc52ac39b98da3e099c57dd27
jacob sanders <jkehsanders@gmail.com>
2019-08-06 16:46:53 -0500

    setup finished

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace lists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> planetList = new List<string>() { "Mercury", "Mars" };
+
+            planetList.Add("Jupiter");
+            planetList.Add("Saturn");
+
+        }
+    }
+}
